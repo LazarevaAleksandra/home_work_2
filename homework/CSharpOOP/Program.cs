34cff3d591969118a3220e3506bd04e6f0569085
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-10-27 17:54:34 +0300

    work on mestakes in CSharpOOP_1

--- homework/CSharpOOP/Program.cs
+++ homework/CSharpOOP/Program.cs
@@ -7,122 +7,116 @@
 //Console.WriteLine($"Итоговая стоимость: {totalPrice}");
 #endregion
 #region Task_2
-//Console.WriteLine("Введите товар, его количество и цену за единицу");
-//string firstItem = Console.ReadLine();
-//var firstNumber = double.Parse(Console.ReadLine());
-//var firstPrice = double.Parse(Console.ReadLine());
-//Console.WriteLine("Введите товар, его количество и цену за единицу");
+using System.Diagnostics;
+
+//Console.WriteLine("Enter the product, its quantity and unit price");
+//string Item = Console.ReadLine();
+//var Number = double.Parse(Console.ReadLine());
+//var Price = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter the product, its quantity and unit price");
 //string secondItem = Console.ReadLine();
 //var secondNumber = double.Parse(Console.ReadLine());
 //var secondPrice = double.Parse(Console.ReadLine());
-//Console.WriteLine("Введите товар, его количество и цену за единицу");
+//Console.WriteLine("Enter the product, its quantity and unit price");
 //string thirdItem = Console.ReadLine();
 //var thirdNumber = double.Parse(Console.ReadLine());
 //var thirdPrice = double.Parse(Console.ReadLine());
-//Console.WriteLine("Введите скидку");
+//Console.WriteLine("Enter discount");
 //var discount = double.Parse(Console.ReadLine());
-//var totalfirst = firstNumber * firstPrice - ((firstNumber * firstPrice) * discount / 100);
-//var totalsecond = secondNumber * secondPrice - ((secondNumber * secondPrice) * discount / 100);
-//var totalthird = thirdNumber * thirdPrice - ((thirdNumber * thirdPrice) * discount / 100);
-//var firstMetod = new Homework_1().SeveralItems(firstItem, firstPrice, discount, 5);
-//var secondMetod = new Homework_1().SeveralItems(secondItem, secondPrice, discount, 5);
-//var thirdMetod = new Homework_1().SeveralItems(thirdItem, thirdPrice, discount, 5);
-//var totalPrice = totalfirst + totalsecond + totalthird;
-//Console.WriteLine(firstMetod);
-//Console.WriteLine(secondMetod);
-//Console.WriteLine(thirdMetod);
-//Console.WriteLine($"Итого к оплате: {totalPrice}");
+//Console.WriteLine(new Homework_1().GetDiscountForNamedItem(Item, Price, discount));
+//Console.WriteLine(new Homework_1().GetDiscountForNamedItem(secondItem, secondPrice, discount););
+//Console.WriteLine(new Homework_1().GetDiscountForNamedItem(thirdItem, thirdPrice, discount););
+//Console.WriteLine($"Total payable: {new Homework_1().GetDiscountItem(Number, Price, secondNumber, secondPrice, thirdNumber, thirdPrice, discount)}");
 #endregion
 #region Task_3
 
-//var optionalFirst = new Homework_1().SeveralItems(firstItem, firstPrice, discount, 5);
-//var optionalSecond = new Homework_1().SeveralItems(secondItem, secondPrice, discount, 5);
-//var optionalThird = new Homework_1().SeveralItems(thirdItem, thirdPrice, discount, 5);
-//var requiredFirst = new Homework_1().SeveralItems(firstItem, firstNumber, firstPrice, discount);
+//var optionalFirst = new Homework_1().SeveralItems(Item, Price, discount);
+//var optionalSecond = new Homework_1().SeveralItems(secondItem, secondPrice, discount);
+//var optionalThird = new Homework_1().SeveralItems(thirdItem, thirdPrice, discount);
+//var requiredFirst = new Homework_1().SeveralItems(Item, Number, Price, discount);
 //var requiredSecond = new Homework_1().SeveralItems(secondItem, secondNumber, secondPrice, discount);
 //var requiredThird = new Homework_1().SeveralItems(thirdItem, thirdNumber, thirdPrice, discount);
 #endregion
 #region Task_4
-//Console.WriteLine("Введите длину первого катета");
-//var firstLeg = double.Parse(Console.ReadLine());
-//Console.WriteLine("Введите длинну второго катета");
-//var secondLeg = double.Parse(Console.ReadLine());
-//Console.WriteLine("Введите высоту");
-//var height = double.Parse(Console.ReadLine());
-//var Area = new Homework_1().Area(firstLeg, secondLeg, height, out double triangleArea, out double quadrilateralArea, out double pentagonArea);
-//Console.WriteLine($"Площадь треугольника: {triangleArea}");
-//Console.WriteLine($"Площадь четырехугольника: {quadrilateralArea}");
-//Console.WriteLine($"Площадь пятиугольника: {pentagonArea}");
-
+//Console.WriteLine("Enter 5 numbers");
+//var numberOne = double.Parse(Console.ReadLine());
+//var numberTwo = double.Parse(Console.ReadLine());
+//var numberThree = double.Parse(Console.ReadLine());
+//var numberFour = double.Parse(Console.ReadLine());
+//var numberFive = double.Parse(Console.ReadLine());
+//Console.WriteLine($"Perimeter of a triangle = {new Homework_1().GetPerimeter(numberOne, numberTwo, numberThree)}");
+//Console.WriteLine($"Perimeter of a quadrilateral = {new Homework_1().GetPerimeter(numberOne, numberTwo, numberThree, numberFour)}");
+//Console.WriteLine($"Perimeter of a pentagon = {new Homework_1().GetPerimeter(numberOne, numberTwo, numberThree, numberFour, numberFive)}");
 #endregion
 #region Task_5
-//Console.WriteLine("Введите длину первого катета");
-//var firstLeg = int.Parse(Console.ReadLine());
-//Console.WriteLine("Введите длинну второго катета");
-//var secondLeg = int.Parse(Console.ReadLine());
-//Console.WriteLine("Введите радиус");
+//Console.WriteLine("Enter the length of the first leg");
+//var firstLeg = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter the length of the second leg");
+//var secondLeg = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter radius");
 //var radius = double.Parse(Console.ReadLine());
-//double pi = Math.PI;
-//var areaTriangl = new Homework_1().AreaOfTriangleAndCircle(firstLeg, secondLeg);
-//var areaCicle = new Homework_1().AreaOfTriangleAndCircle(radius, pi);
-//Console.WriteLine($"Площадь треугольника: {areaTriangl}");
-//Console.WriteLine($"Площадь окружности: {areaCicle}");
+//Console.WriteLine($"Area of ​​a triangle = {new Homework_1().GetAreaOfTriangleAndCircle(firstLeg, secondLeg)}");
+//Console.WriteLine($"Area of ​​a circle: {new Homework_1().GetAreaOfTriangleAndCircle(radius)}");
 
 #endregion
 #region Task_6
 //Инкапсуляция:
 //    Ребенка очень строго воспитывают в семье, но он не рассказывает об этом окружающим.
+//    Телефон, у которого есть функция "позвонить(номер телефона)" пользователю, не обязательно знать,
+//    как "летают" электроны внутри, он пользуется готовой функцией.
 //Наследование:
 //    Ребенок унаследовал цвет волос и глаз от родителей, но характер - его собственный.
 //Полиморфизм:
 //     Я однажды получила штраф за то, что под моими ФИО попал в дтп другой человек. 
 //     У того человека были поддельные права на мое имя. А у меня прав не было на тот момент.
+//     Актер в театре может примерять на себя много ролей - или принимать "много форм".
 //Абстрагирование:
 //    Ребенок рисует дом. Стены и крышу. Но не прорисовывает мелкие детали (дверь, окна).
 
 #endregion
 public class Homework_1
 {
-
     //public double GetDiscount(double numberOfItems, double price, double discount)
     //{
-    //    var itemsPrice = numberOfItems * price;
-    //    var totalPrice = itemsPrice - (itemsPrice * discount / 100);
-    //    return totalPrice;
-    //}
-    //public string SeveralItems(string firstItem, double firstNumber, double firstPrice, double discount)
-    //{
-    //    var firstChek = firstNumber * firstPrice;
-    //    var totalFirst = firstChek - (firstChek * discount / 100);
-    //    return $" Наименование:{firstItem}, количество:{firstNumber}, цена:{firstPrice}, цена со скидкой:{totalFirst}";
-    //}
-    //public string SeveralItems(string firstItem, double firstPrice, double discount, double firstNumber = 5)
-    //{
-    //    var firstChek = firstNumber * firstPrice;
-    //    var totalFirst = firstChek - (firstChek * discount / 100);
-    //    return $" Наименование:{firstItem}, количество:{firstNumber}, цена:{firstPrice}, цена со скидкой:{totalFirst}";
+    //    var totalChek = numberOfItems * price - ((numberOfItems * price) * discount / 100);
+    //    return totalChek;
     //}
 
-    //public double Area (double firstLeg, double secondLeg, double height, out double triangleArea, out double quadrilateralArea, out double pentagonArea)
+    //public double GetDiscountItem(double Number, double Price, double secondNumber, double secondPrice, double thirdNumber, double thirdPrice, double discount)
     //{
-    //    triangleArea = firstLeg * secondLeg / 2;
-    //    quadrilateralArea = firstLeg * 4;
-    //    pentagonArea = triangleArea * 5;
-    //    return triangleArea;
-    //    return quadrilateralArea;
-    //    return pentagonArea;
+    //    var totalfirst = new Homework_1().GetDiscount(Number, Price, discount);
+    //    var totalsecond = new Homework_1().GetDiscount(secondNumber, secondPrice, discount);
+    //    var totalthird = new Homework_1().GetDiscount(thirdNumber, thirdPrice, discount);
+    //    var totalPrice = totalfirst + totalsecond + totalthird;
+    //    return totalPrice;
     //}
-    
-    //public double AreaOfTriangleAndCircle (int firstLeg, int secondLeg)
+    //public string GetDiscountForNamedItem(string Item, double Price, double discount, double Number = 5)
+    //{
+    //    var totalFirst = new Homework_1().GetDiscount(Number, Price, discount);
+    //    return $" Наименование:{Item}, количество:{Number}, цена:{Price}, цена со скидкой:{totalFirst}";
+    //}
+
+
+    //public double GetPerimeter(params double[] numberLenght)
+    //{
+    //    double perimeter = 0;
+    //    foreach (double number in numberLenght)
+    //    {
+    //    perimeter += number;
+    //    }
+    //    return perimeter;
+    //}
+
+    //public double GetAreaOfTriangleAndCircle(double firstLeg, double secondLeg)
     //{
     //    var triangleArea = firstLeg * secondLeg / 2;
     //    return triangleArea;
     //}
-    //public double AreaOfTriangleAndCircle (double radius, double pi)
+    //public double GetAreaOfTriangleAndCircle(double radius)
     //{
-    //    var cicleArea = radius * 2 * pi;
+    //    var cicleArea = radius * 2 * Math.PI;
     //    return cicleArea;
-           
+
     //}
 
 
