1fd40d4055dc9e46ed410d7372bf826e9276a425
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-10-30 18:04:59 +0300

    CSharpOOP2

---
+++ homework/CSharpOOP2/Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP2
+{
+    public class Car //Task_1
+    {
+        public string Brand; //Task_2
+        public string _Color;
+        public int YearOfIssue;
+        public double _DiskSize;
+        public double _Price;
+        public static string Type { get; } ="Transport";  //Task_17
+
+        public static void GreetCar() => Console.WriteLine("Hello!"); //Task_18
+
+        public double _defaultPrice = 30.000;  //Task_13
+        public double Price
+        {
+            get
+            {
+                return _Price;
+            }
+            set
+            {
+                _Price = _defaultPrice;
+            }
+        }
+        /*public double YearOfIssue { get => _YearOfIssue = 2000; } *///Task_14
+        public double DiskSize { set => _DiskSize = 7; }   //Task_14
+
+        public string Color { get; internal set; } //Task_16
+
+
+        public void CheckBrand() //Task_3
+        {
+            if (Brand.Equals("BMW"))
+            {
+                Console.WriteLine("Car brand - BMW");
+            }
+            else if (Brand.Equals("Audi"))
+            {
+                Console.WriteLine("Car brand - Audi");
+            }
+            else
+            {
+                Console.WriteLine("Other car brand");
+            }
+        }
+        public Car(string brand, string color, int yearofissue, double diskSize, double price) //Task_6
+        {
+            Brand = brand;
+            Color = color;
+            YearOfIssue = yearofissue;
+            DiskSize = diskSize;
+            Price = price;
+        }
+        public Car() //Task_7
+        {
+        }
+        public Car(string brand, int yearofissue) //Task_8
+        {
+            Brand = brand;
+            YearOfIssue = yearofissue;
+        }
+        internal void CheckYearOfIssue() //Task_11
+        {
+            if (YearOfIssue <= 2020)
+            {
+                Console.WriteLine("Car is old");
+            }
+            else
+            {
+                Console.WriteLine("Car is young");
+            }
+        }
+        private void CheckPrice() //Task_11
+        {
+            if (Price <= 20.000)
+            {
+                Console.WriteLine("The car is cheap");
+            }
+            else
+            {
+                Console.WriteLine("Expensive car");
+            }
+        }
+    }
+
+}
---
+++ homework/CSharpOOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using CSharpOOP2;
+using System.Diagnostics;
+
+Car youngCar = new Car  //Task_4
+{
+    Brand = "BMW",
+    Color = "Black",
+    YearOfIssue = 2022,
+    DiskSize = 5,
+    Price = 20.000
+};
+
+Car oldCar = new Car { Brand = "Audi", Color = "Red", YearOfIssue = 2020, DiskSize = 3, Price = 10.000 };  //Task_5
+oldCar.CheckBrand();  //Task_10
+
+Car perfectCar = new Car("Lamborghini", "Gold", 2020, 10, 50.000); //Task_9
+
+perfectCar.CheckYearOfIssue();  //Task_11
+///*perfectCar.CheckPrice(); */ //Task_11  //Error Line
+
+//Car overCar = new Car { Color = "White", YearOfIssue = 2018, DiskSize = 7, Price = 30.000 };
+///*overCar.CheckBrand();*/  // Task_12  //Error Line
+
+Car defaultItem = new Car { Brand = "BMW", Color = "Black", YearOfIssue = 2022, DiskSize = 5 }; //Task_13
+defaultItem.Price = 30.000;
+Console.WriteLine($"Car cost: {defaultItem.Price}");
+
+//Console.WriteLine($"{defaultItem.YearOfIssue}, {defaultItem.DiskSize}"); //Task_15  //Error Line
+
+var type = Car.Type; //Task_17
+Car.GreetCar(); //Task_18
+
+Assistant.Brand("BMW", 5, 20.000); //Task_20
+Assistant.YearOfIssue(youngCar);
+Assistant.Price(oldCar);
