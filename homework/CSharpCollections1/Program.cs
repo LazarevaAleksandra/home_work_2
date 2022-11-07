6b0c6e5a8aecba1b950a74dd37ec7212e32d95ea
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-11-07 14:00:31 +0300

    commit

---
+++ homework/CSharpCollections1/Program.cs
@@ -0,0 +1,50 @@
+﻿#region Task_1
+//int[] array = new int[] { 4, 32, 89, 22, 56, 17, 7 };
+
+//Array.Sort(array);
+
+//Console.WriteLine("Array of even numbers:");
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        Console.Write("{0} ", array[i]);
+//    }
+//}
+//Console.WriteLine("\nArray of odd numbers:");
+
+//for (int j = 0; j < array.Length; j++)
+//{
+//    if (array[j] % 2 != 0)
+//    {
+//        Console.Write("{0} ", array[j]);
+//    }
+//}
+#endregion
+#region Task_2
+//using System;
+
+//Random random = new Random();
+//double[] array = new double[10];
+
+//Console.WriteLine("Array:");
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    array[i] = random.Next(-20, 20);
+//    Console.Write("{0} ", array[i]);
+//}
+//Console.WriteLine();
+
+//double max = array.Max();
+//double min = array.Min();
+//double difference = max - min;
+//Console.WriteLine($"Max = {max}");
+//Console.WriteLine($"Min = {min}");
+//Console.WriteLine($"Difference of max and min = {difference}");
+#endregion
+#region Task_3
+
+#endregion
+
