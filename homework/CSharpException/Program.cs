f6c39e41f1316ac9650c62b53ed01c2c9a5e1699
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-11-14 19:35:59 +0300

    comments

--- homework/CSharpException/Program.cs
+++ homework/CSharpException/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpException;
+
+Triangle triangle = new Triangle();
+
+//Task_1
+//try
+//{
+//    Console.WriteLine("Enter the lengths of all sides:");
+//    triangle.OneLength = int.Parse(Console.ReadLine());
+//    triangle.TwoLenght = int.Parse(Console.ReadLine());
+//    triangle.ThreeLenght = int.Parse(Console.ReadLine());
+
+//    triangle.SideLengthCheck();
+//}
+//catch (FormatException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//finally
+//{
+//    Console.WriteLine("Condition violation!");
+//}
+
+//Task_2
+try
+{
+    Console.WriteLine("Enter the lengths of all sides:");
+    triangle.OneLength = int.Parse(Console.ReadLine());
+    triangle.TwoLenght = int.Parse(Console.ReadLine());
+    triangle.ThreeLenght = int.Parse(Console.ReadLine());
+
+    triangle.CheckingValuesForZero();
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message);  
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ homework/CSharpException/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpException
+{
+    public class Triangle
+    {
+        public int OneLength { get; set; }
+        public int TwoLenght { get; set; }
+        public int ThreeLenght { get; set; }
+
+        public Triangle()
+        {
+        }
+        public Triangle(int oneLength, int twoLenght, int threeLenght)
+        {
+            OneLength = oneLength;
+            TwoLenght = twoLenght;
+            ThreeLenght = threeLenght;
+        }
+
+        public void SideLengthCheck()
+        {
+            if (OneLength < TwoLenght + ThreeLenght
+                || TwoLenght < OneLength + ThreeLenght 
+                || ThreeLenght < OneLength + TwoLenght) 
+            {
+                throw new Exception("The length of a side is greater than the sum of the other two!");
+            }
+        }
+
+        public void CheckingValuesForZero()
+        {
+            if (OneLength != 0
+                || TwoLenght != 0
+                || ThreeLenght != 0)
+            {
+                throw new Exception("The side length is zero!");
+            }
+        }
+    }
+}
