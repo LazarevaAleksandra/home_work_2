93511bc1481f2aea761cf401d6e58ecaa115c25f
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-11-24 18:37:27 +0300

    CSharpAdvanced_1

---
+++ homework/CSharpAdvanced/HelperClass.cs
@@ -0,0 +1,35 @@
+﻿using CSharpOOP2;
+namespace CSharpAdvanced
+{
+    public static class HelperClass
+    {
+        public static void PrintBrand(this Car youngCar, double DiskSize = 5, double Price = 20.000)
+        {
+            Console.WriteLine($"DiskSize {DiskSize}, Price {Price}");
+        }
+
+        public static void PrintYearOfIssue(this Car youngCar, int newYearOfIssue = 2000)
+        {
+            youngCar.YearOfIssue = newYearOfIssue;
+            Console.WriteLine(newYearOfIssue);
+        }
+
+        public static void PrintPrice(this Car oldcar, double newPrice = 10.000)
+        {
+            if (oldcar.Price == 10.000)
+            {
+                Console.WriteLine($"Car is {oldcar.Brand}");
+            }
+            else
+            {
+                Console.WriteLine("Other car");
+            }
+        }
+
+        public static List<T> GetCarBrand<T>(this List<T> cars) where T : Car
+        {
+            return cars.Where(x => x.Brand.Length > 2).ToList();
+
+        }
+    }
+}
--- homework/CSharpAdvanced/Program.cs
+++ homework/CSharpAdvanced/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CSharpAdvanced;
+using CSharpOOP2;
 
 #region Task_1
+
 GenericMetod genericMetod = new GenericMetod();
 
 int[] intItems = new int[] { 1, 2, 3, 4, 5 };
@@ -10,9 +12,11 @@ string[] stringItems = new string[] { "Hello", "world" };
 genericMetod.GetFirstItem(intItems);
 genericMetod.GetFirstItem(doubleItems);
 genericMetod.GetFirstItem(stringItems);
+
 #endregion
 
 #region Task_2
+
 Flower rose = new Rose("Rose", "Black", 10);
 Flower chamomile = new Chamomile("Chamomile", "White", 5);
 
@@ -41,9 +45,11 @@ double FinalCheck<T>(T flower, T number)
 {
     return GetPrice(rose, numberOfRose) + GetPrice(chamomile, numberOfChamomile);
 }
+
 #endregion
 
 #region Task_3
+
 Flower newRose = new Rose("Rose", "Black", 10);
 Flower newChamomile = new Chamomile("Chamomile", "White", 5);
 
@@ -66,6 +72,21 @@ Console.WriteLine($"Name: {newRose.Name}; {printNewColorRose.GetNewColor(newRose
 
 Console.WriteLine($"Name: {newChamomile.Name}; {printNewColorChamomile.GetNewColor(newChamomile, colorOfChamomile)}; " +
     $"Price after repaint: {printNewColorChamomile.GetNewPrice(newChamomile)}");
+
 #endregion
 
+#region Task_4
+
+Car oneCar = new Car("BMW", "Black", 2022, 10, 50.000);
+Car twoCar = new Car("Audi", "White", 2000, 5, 30.000);
+Car threeCar = new Car("VW", "Blue", 1996, 3, 15.000);
+
+oneCar.PrintYearOfIssue();
+oneCar.PrintPrice();
+oneCar.PrintBrand();
+
+var carList = new List<Car>() { oneCar, twoCar, threeCar };
+carList.GetCarBrand();
+
+#endregion
 
