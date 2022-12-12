be98c94045f548fdc1bfcd1571b24b482b7791bf
Aleksandra Lazareva <aleksandrapavlovskaya2@gmail.com>
2022-12-13 01:22:59 +0300

    work with new comments in unit_testing_frameworks

--- homework/CalculatorNUnitTests/SquareRootTests.cs
+++ homework/CalculatorNUnitTests/SquareRootTests.cs
@@ -6,14 +6,20 @@ namespace CalculatorNUnitTests
     [TestFixture]
     public class SquareRootTests
     {
+        private Calculator _squareRoot;
+
+        [SetUp]
+        public void SquareRootInicialize()
+        {
+            _squareRoot = new Calculator();
+        }
         [Test]
         public void SquareRootTest()
         {
             double numberOne = 5;
             double expectedResult = 2.23606797749979;
 
-            var squareRoot = new Calculator();
-            double result = squareRoot.Sqrt(numberOne);
+            double result = _squareRoot.Sqrt(numberOne);
 
             Assert.AreEqual(expectedResult, result);
         }
@@ -24,9 +30,7 @@ namespace CalculatorNUnitTests
             double numberOne = -5;
             var expectedResult = typeof(Exception);
 
-            var squareRoot = new Calculator();
-
-            Assert.Throws(expectedResult, () => squareRoot.Sqrt(numberOne));
+            Assert.Throws(expectedResult, () => _squareRoot.Sqrt(numberOne));
         }
     }
 }
--- homework/CalculatorNUnitTests/SubstractionTests.cs
+++ homework/CalculatorNUnitTests/SubstractionTests.cs
@@ -6,18 +6,24 @@ namespace CalculatorNUnitTests
     [TestFixture]
     public class SubtractionTests
     {
-        //The test failed on the subtraction of fractional numbers
-        //(Expected:<2,4>. Actual:<2,4000000000000004>).
-
+        private Calculator _substraction;
+        
+        [SetUp]
+        public void SubstractionInicialize()
+        {
+            _substraction = new Calculator();
+        }
         [Test]
         public void SubstractionTest()
         {
+            //The test failed on the subtraction of fractional numbers
+            //(Expected:<2,4>. Actual:<2,4000000000000004>).
+
             double numberOne = 5.4;
             double numberTwo = 3;
             double expectedResult = 2.4;
 
-            var substraction = new Calculator();
-            double result = substraction.Sub(numberOne, numberTwo);
+            double result = _substraction.Sub(numberOne, numberTwo);
 
             Assert.AreEqual(expectedResult, result);
         }
@@ -28,9 +34,7 @@ namespace CalculatorNUnitTests
         public void SubtractionOfNegativeNumbersTest(double numberOne,
             double numberTwo, double expectedResult)
         {
-            var substract = new Calculator();
-
-            double result = substract.Sub(numberOne, numberTwo);
+            double result = _substraction.Sub(numberOne, numberTwo);
 
             Assert.AreEqual(expectedResult, result);
         }
